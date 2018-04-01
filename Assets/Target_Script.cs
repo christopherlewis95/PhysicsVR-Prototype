da3e44ae67f57dd3ce27677c4e69c783132c689a
wzandbergen <wzandbergen@nevada.unr.edu>
2018-04-01 14:44:11 -0700

    Targets, Lighting, Controller
    - Target asset created
    - Targets are destroyed on collision with cannonball
    - Particles are temp implemented, but turned off
    
    BUGS:
    - Attempted to fix Left controller issue (Still not working)
    - When loading from the Test menu into the prototype, directional lighting is a MASSIVE issue, causing problems shadows
    
    Co-Authored-By: Andrew Munoz <amunoz247@users.noreply.github.com>
    Co-Authored-By: Nicholas Bolling <nicktendo13@users.noreply.github.com>

---
+++ Assets/Target_Script.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target_Script : MonoBehaviour {
+	public GameObject target;
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Cannonball") {
+			Destroy (target);
+		}
+	}
+}
