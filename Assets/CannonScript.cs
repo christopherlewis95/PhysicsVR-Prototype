bfd9a1d19fb0ad8e12c85a06caa9b9aa00d5238d
christopherlewis95 <christopherlewis@nevada.unr.edu>
2018-04-09 15:04:31 -0700

    Added ship
    Ship is in the game, working on quaternions.

--- Assets/CannonScript.cs
+++ Assets/CannonScript.cs
@@ -17,20 +17,28 @@ public class CannonScript : MonoBehaviour {
 	ParticleSystem particleExplosion;
 	private bool played;
 
+	public GameObject Base;
+	//public GameObject intit
 	public Transform firepoint;
 	public GameObject cannonBall;
-	float currentAngleX = 0;
-	float previousAngleX = 0;
-	float currentAngleY = 0;
-	float previousAngleY = 0;
-	float xInitial = 0;
-	float yInitial = 0;
+	float currentAngleX;
+	float previousAngleX;
+	float currentAngleY;
+	float previousAngleY;
+	float xInitial;
+	float yInitial;
 	float sInitial = 0;
 	float strength;
 	float pstrength;
 	float maxStrength = 100;
 	// Use this for initialization
 	void Start () {
+
+
+		xInitial =  Base.transform.rotation.x;
+		yInitial = Base.transform.rotation.y;
+		//sInitial = this.gameObject.transform.rotation.z;
+		
 		HLever= GameObject.FindGameObjectWithTag("HLever");
 		hLeverData = HLever.GetComponent<HLeverScript> ();
 		 VLever = GameObject.FindGameObjectWithTag("VLever");
@@ -57,14 +65,14 @@ public class CannonScript : MonoBehaviour {
 			print (currentAngleX);
 			previousAngleX = currentAngleX;
 			currentAngleX = currentAngleX + xInitial;
-			this.transform.eulerAngles = new Vector3(currentAngleX, -currentAngleY, 0);
+			Base.transform.eulerAngles = new Vector3(currentAngleX, -currentAngleY, 0);
 			//this.transform.Rotate (currentAngleX, 0, 0);
 		}
 		else if (currentAngleY != previousAngleY) {
 			previousAngleY = currentAngleY;
 			currentAngleY = currentAngleY + yInitial;
 			print (currentAngleY);
-			this.transform.eulerAngles = new Vector3(currentAngleX, -currentAngleY, 0);
+			Base.transform.eulerAngles = new Vector3(currentAngleX, -currentAngleY, 0);
 			//this.transform.Rotate (0 ,currentAngleY, 0);
 		}
 
